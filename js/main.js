f90f7c1f2adcb2379a76ede08d37a060cc1bbc6b
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-04-25 19:45:57 +0500

    another mobile fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 (() => {
-	"use strict";
-	window, document, document.documentElement, document.body;
+	var e = {
+			451: () => {
+				alert("ширина " + window.innerWidth + ", высота " + window.innerHeight);
+			},
+		},
+		t = {};
+	function n(r) {
+		var o = t[r];
+		if (void 0 !== o) return o.exports;
+		var i = (t[r] = { exports: {} });
+		return e[r](i, i.exports, n), i.exports;
+	}
+	(() => {
+		"use strict";
+		window, document, document.documentElement, document.body, n(451);
+	})();
 })();
