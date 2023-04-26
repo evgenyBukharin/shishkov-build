9166110f389a6095a842261fd2fa16d5da22bdd8
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-04-26 09:22:54 +0500

    debug

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-
+(()=>{var e={451:()=>{alert("ширина "+window.innerWidth+", высота "+window.innerHeight)}},t={};function n(r){var o=t[r];if(void 0!==o)return o.exports;var i=t[r]={exports:{}};return e[r](i,i.exports,n),i.exports}(()=>{"use strict";window,document,document.documentElement,document.body,n(451)})()})();
